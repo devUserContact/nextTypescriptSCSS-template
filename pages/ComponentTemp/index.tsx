ed7fc920caa6b50c586fe7747b479d3f94ad22f8
devUserContact <developer.user.contact@protonmail.com>
2022-03-16 22:30:13 -0700

    initial commit

---
+++ pages/ComponentTemp/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import styles from '../../styles/Home.module.scss'
+
+const ComponentTemp: NextPage = () => {
+  
+	return (
+		<>
+			<h1 className={styles.title}>
+				nextTypescriptSCSS-template
+			</h1>
+			<p></p>
+			<div className={styles.container}>
+			</div>
+			<p></p>
+			<div className={styles.container}>
+				<div className={styles.grid}>
+					<div className={styles.gridWrapper}>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+export default ComponentTemp
